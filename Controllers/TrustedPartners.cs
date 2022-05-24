6582fdd3085e0886434748b02015b9c13fd68db2
Haim <Haim@HAYOGA>
2022-05-24 23:12:49 +0300

    Add project files.

---
+++ Controllers/TrustedPartners.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrustedPartners.Attributes;
+using TrustedPartners.DataAccessLayer;
+using Microsoft.Extensions.Configuration;
+
+namespace TrustedPartners.Controllers
+{
+    //The following attribute enables ApiKey validation
+    [ApiKey]
+    [ApiController]
+    [Route("[controller]")]
+    public class TrustedPartnersController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        
+        private readonly ILogger<TrustedPartnersController> _logger;
+        public TrustedPartnersController(ILogger<TrustedPartnersController> logger)
+        {
+            _logger = logger;
+        }
+        
+        //The AGENT_CODE who has the highest sum of ADVANCE_AMOUNT in the first quarter of the specific year sent in the parameter
+
+        [HttpGet("HighestAgent/{year}")]
+        public ActionResult<string> HighestAgent(int year)
+        {
+            var da = new DataAccess();
+            return da.HighestAgent(year);
+        }
+
+        // GET api/WeatherForecast/GetListOfAgents/
+        [HttpGet("GetListOfAgents")]
+        public ActionResult<string> GetListOfAgents([FromQuery] string[] agents, [FromQuery] int number)
+        {
+            var da = new DataAccess();
+            return da.GetListOfAgents(agents, number);
+        }
+
+        // GET api/GetTopAgents/
+        [HttpGet("{number}/{year}")]
+        public ActionResult<string> Get(int number, int year)
+        {
+            var da = new DataAccess();
+            return da.GetTopAgents(number, year);
+        }
+    }
+}
---
+++ DataAccess/DataAccessLayer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TrustedPartners.DataAccessLayer
+{
+    public class DataAccess
+    {
+        private static string ConnString = ConfigurationManager.ConnectionStrings["TrustedPartners"].ConnectionString;
+    
+        //The AGENT_CODE who has the highest sum of ADVANCE_AMOUNT in the first quarter of the specific year sent in the parameter
+        //The result comes already in JSON format from SQL Server DB
+        public string HighestAgent(int year)
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))            
+                using (SqlCommand cmd = new SqlCommand("HighestAgentFistQuarter", con))
+                {
+                    try
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@year", SqlDbType.Int).Value = year;
+                        con.Open();
+                        var response = (string)cmd.ExecuteScalar();
+
+                        return response;
+                    }
+                    catch(Exception e)
+                    {
+                        return e.Message; //Failed
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+                }
+        }
+        public string GetListOfAgents(string[] agents, int number)
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand cmd = new SqlCommand("GetListOfAgents", con))
+            {
+                try
+                {
+                    var agentList = string.Join(',', agents);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@agentList", SqlDbType.NVarChar).Value = agentList;
+                    cmd.Parameters.AddWithValue("@number", SqlDbType.Int).Value = number;
+                    con.Open();
+                    var response = (string)cmd.ExecuteScalar();
+
+                    return response;
+                }
+                catch (Exception e)
+                {
+                    return e.Message; //Failed
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+        public string GetTopAgents(int number, int year)
+        {
+            using (SqlConnection con = new SqlConnection(ConnString))
+            using (SqlCommand cmd = new SqlCommand("GetTopAgents", con))
+            {
+                try
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@number", SqlDbType.Int).Value = number;
+                    cmd.Parameters.AddWithValue("@year", SqlDbType.Int).Value = year;
+                    con.Open();
+                    var response = (string)cmd.ExecuteScalar();
+
+                    return response;
+                }
+                catch (Exception e)
+                {
+                    return e.Message; //Failed
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
